d47f42fde2500f56dc3419bde8d2a697a48bd2fc
libny limas <libny.limas@gmail.com>
2014-11-17 20:05:11 -0600

    agragando clases para generar la base de datos

--- src/SistemaZoologico.Dominio/Entidades/TipoBoleto.cs
+++ src/SistemaZoologico.Dominio/Entidades/TipoBoleto.cs
@@ -1,5 +1,27 @@
-﻿namespace SistemaZoologico.Dominio.Entidades
+﻿using System.Collections.Generic;
+
+namespace SistemaZoologico.Dominio.Entidades
 {
+    public class Venta : IEntidad
+    {
+        public virtual Cliente Cliente { get; set; }
+        public virtual int Id { get; set; }
+
+        public virtual IEnumerable<DetalleVenta> Detalle { get; set; } 
+    }
+
+    public class DetalleVenta:IEntidad
+    {
+        public virtual int Id { get; set; }
+    }
+
+    public class Cliente : IEntidad
+    {
+        public virtual string Nombre { get; set; }
+        public virtual string Telefono { get; set; }
+        public virtual int Id { get; set; }
+    }
+
     public class TipoBoleto : IEntidad
     {
         protected TipoBoleto()
